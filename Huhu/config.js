8b96eb278f20f2a6bc3541ccad567b3d22e8661b
malcolmmd10-oss <malcolmmd10@gmail.com>
2025-12-19 21:14:19 +0800

    Add files via upload

---
+++ Huhu/config.js
@@ -0,0 +1,9 @@
+// Configuration file - Change settings here easily
+const CONFIG = {
+    // Change this URL to use a different background music
+    // Place your MP3 file in the music/ folder and update the filename here
+    MUSIC_URL: 'music/blue.mp3',
+    
+    // Music volume (0 to 1)
+    MUSIC_VOLUME: 0.25
+};
